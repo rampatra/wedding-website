db9993fa2dbc27e9768f69bf511dc1d000a9f260
Axel Borgdén <axel.borgden@cygni.se>
2021-10-18 21:02:21 +0200

    test edit

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 59.604352864811965, lng: 17.049652783639235};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
