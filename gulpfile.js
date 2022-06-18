7e8a97afe539db2ee1e17891ef64e5141aec9570
Canaan Gifford <canaangifford@gmail.com>
2022-06-18 12:41:21 -0400

    updating node package versions for 2022

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
--- node_modules/gulp-rename/index.js
+++ node_modules/gulp-rename/index.js
@@ -4,13 +4,14 @@ var Stream = require('stream');
 var Path = require('path');
 
 function gulpRename(obj, options) {
-
   options = options || {};
 
-  var stream = new Stream.Transform({objectMode: true});
+  var stream = new Stream.Transform({ objectMode: true });
 
   function parsePath(path) {
-    var extname = options.multiExt ? Path.basename(path).slice(Path.basename(path).indexOf('.')) : Path.extname(path);
+    var extname = options.multiExt
+      ? Path.basename(path).slice(Path.basename(path).indexOf('.'))
+      : Path.extname(path);
     return {
       dirname: Path.dirname(path),
       basename: Path.basename(path, extname),
@@ -18,26 +19,26 @@ function gulpRename(obj, options) {
     };
   }
 
-  stream._transform = function (originalFile, unused, callback) {
-
-
-    var file = originalFile.clone({contents: false});
+  stream._transform = function(originalFile, unused, callback) {
+    var file = originalFile.clone({ contents: false });
     var parsedPath = parsePath(file.relative);
     var path;
 
     var type = typeof obj;
 
     if (type === 'string' && obj !== '') {
-
       path = obj;
-
     } else if (type === 'function') {
+      let newParsedPath = obj(parsedPath, file);
+      if (typeof newParsedPath === 'object' && newParsedPath !== null) {
+        parsedPath = newParsedPath;
+      }
 
-      obj(parsedPath, file);
-      path = Path.join(parsedPath.dirname, parsedPath.basename + parsedPath.extname);
-
+      path = Path.join(
+        parsedPath.dirname,
+        parsedPath.basename + parsedPath.extname
+      );
     } else if (type === 'object' && obj !== undefined && obj !== null) {
-
       var dirname = 'dirname' in obj ? obj.dirname : parsedPath.dirname,
         prefix = obj.prefix || '',
         suffix = obj.suffix || '',
@@ -45,12 +46,12 @@ function gulpRename(obj, options) {
         extname = 'extname' in obj ? obj.extname : parsedPath.extname;
 
       path = Path.join(dirname, prefix + basename + suffix + extname);
-
     } else {
-
-      callback(new Error('Unsupported renaming parameter type supplied'), undefined);
+      callback(
+        new Error('Unsupported renaming parameter type supplied'),
+        undefined
+      );
       return;
-
     }
 
     file.path = Path.join(file.base, path);
