d9b35807418c4c14ec68c437289d72c5fefd5481
Ram <2862724+rampatra@users.noreply.github.com>
2023-02-27 23:30:05 +0000

    Delete node_modules directory

--- node_modules/sass/sass.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var library = require('./sass.dart.js');
-library.load({
-  readline: require("readline"),
-  chokidar: require("chokidar"),
-});
-
-library.cli_pkg_main_0_(process.argv.slice(2));
