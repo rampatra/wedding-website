d3891323616e784d74ca490816dbb12e221963b5
Hector Palacios <hectorpal@gmail.com>
2023-12-21 18:56:06 -0500

    script url

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxWVTfgmn-gfdpCybL8RYQ9Bvz9CoU2eEnT0TZxoHhY_vaO_QAKGgYFu9hSrwsgV8Ll/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
