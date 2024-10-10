10eeeed7ccf28dfa0fdc9a63661805bba52eab9c
alex89cec <amedinar89@gmail.com>
2024-10-09 21:00:58 -0300

    test

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> El Codigo no es el correcto ! .'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbygxIKr9RiUxNlGetBsttWIw_YiQVTraXZqVKUFoEGvWZcZrmNOXMnT4MHkQQQ8wp0O8w/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwI6l-ESqgz8M6eqavRA9nK6wn3WCaJrTcnsdtATtAjvug5pcd6iExy5EOTInpxfNyV9Q/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
