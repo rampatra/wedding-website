e33d8be5b267e47923d76cc62b9a83c7d6246fcb
Gabor Havasi <havasi.gaabor@gmail.com>
2025-01-22 18:41:16 +0100

    add maps

--- js/scripts.js
+++ js/scripts.js
@@ -236,29 +236,14 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = { lat: 47.335942043841015, lng: 19.418992327143737 };
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: location,
-        scrollwheel: false
+        zoom: 14,
+        center: location
     });
 
     var marker = new google.maps.Marker({
         position: location,
-        map: map
-    });
-}
-
-function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: la_fiesta,
-        scrollwheel: false
-    });
-
-    var marker = new google.maps.Marker({
-        position: la_fiesta,
         map: map
     });
 }
